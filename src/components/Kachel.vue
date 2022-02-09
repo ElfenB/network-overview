1b0d9858c9531edccaa23f8bec53e8f83e94939f
Benjamin Elfen <benjamin@elfen-online.de>
2022-02-09 18:26:09 +0100

    added mongoose version support

--- src/components/Kachel.vue
+++ src/components/Kachel.vue
@@ -4,7 +4,7 @@
       <h2>{{ data.name }}</h2>
       <p v-if="data.room">{{ data.room }}</p>
       <h3>{{ data.description }}</h3>
-      <p v-if="details.version != ''">{{ details.version }}{{ details.sensors ? "s" : "" }}</p>
+      <p v-if="details">{{ details.version }}{{ details.sensors ? "s" : "" }}</p>
     </div>
   </a>
 </template>
@@ -18,20 +18,28 @@ const props = defineProps({
   type: String
 })
 
-const details = ref({})
+const details = ref()
 
 async function getDetails(url, type) {
   if (type == "tasmota") {
     return await getDetailsTasmota(url)
+  } else if (type == "mongoose") {
+    return await getDetailsMongoose(url)
   }
   return null
+}
+
+async function getDetailsMongoose(url) {
+  let only_ip = url.substring(url.indexOf("http://") + 7)
+
+  const res = await axios.get("http://pi4.fritz.box:1880/mongoose_details/" + only_ip)
+  return res.data
 }
 
 async function getDetailsTasmota(url) {
   let only_ip = url.substring(url.indexOf("http://") + 7)
 
-  const res = await axios.get("http://pi4.fritz.box:1880/tasmota_version/" + only_ip)
-  console.log(res.data);
+  const res = await axios.get("http://pi4.fritz.box:1880/tasmota_details/" + only_ip)
   return res.data
 }
 
